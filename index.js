f3e7c4d179e85be4569584bd3a7a21c04eb5e242
LiamSo121 <liam121300@gmail.com>
2026-03-02 22:19:18 +0200

    finalize ci/cd

--- index.js
+++ index.js
@@ -149,8 +149,19 @@ function startBot(config) {
     const client = new Client({
         authStrategy: new LocalAuth({ clientId: resolvedConfig.id }),
         puppeteer: {
+            executablePath: '/usr/bin/google-chrome-stable',
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process',
+                '--disable-gpu'
+            ],
+            protocolTimeout: 60000
         }
     });
 
